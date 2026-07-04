14d7cf1961317e90d4f39be7ec3cade452f7ba0a
Filipcihlar <filipcihlar11@seznam.cz>
2026-07-04 11:11:23 +0200

    Update cwg.js

--- cwg/cwg.js
+++ cwg/cwg.js
@@ -438,5 +438,13 @@ window.cwg = [
     "place": "Cyklostezka 3A",
     "date": "2026/07/03",
     "info": ""
+  },
+  {
+    "name": "pavelchodov",
+    "year": "2024",
+    "type": "CWG",
+    "place": "Cyklostezka 3A",
+    "date": "2026/07/03",
+    "info": ""
   }
 ]
